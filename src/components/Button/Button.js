7853894154147a4e4ca4a7ed123d0c5c96f99197
Hotmann <hotmann442@gmail.com>
2023-10-08 15:09:44 +0100

    fix: resume download

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 import styles from "./styles.module.css";
 
-const Button = ({ children, to, download }) => {
+const Button = ({ children, to, ...others }) => {
   return (
-    <Link download={download} className={styles.link} to={to}>
+    <Link className={styles.link} to={to} {...others}>
       {children}
     </Link>
   );
